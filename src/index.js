58a652edcadaccb69f3ad4838a7e4a7775dce8e6
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-02-10 16:54:32 -0600

    implementando botones

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+var video = document.getElementById('video');
 
-console.log('Happy hacking :)')
+play.addEventListener('click', () => {
+    if (video.paused)
+        video.play();
+    else
+        video.pause();
+});
+
+
+var recargar = document.getElementById('recargar');
+
+recargar.addEventListener('click', () => {
+    video.load();
+});
+
+var completo = document.getElementById('completo');
+completo.addEventListener('click', () => {
+    video.requestFullscreen();
+})
+
+function skip() {
+    $('.skip_button').on('click', function() {
+        var vid = $("#video1")[0];
+        var vidDuration = Math.floor(vid.duration);
+        skipTime(vidDuration);
+        console.log(vidDuration);
+
+    })
+
+}
+
+function skipTime(time) {
+    var vid = $("#video1")[0];
+    vid.play();
+    vid.pause();
+    vid.currentTime = time;
+    vid.play();
+    console.log(vid.duration);
+};
+
+$(function() {
+
+    skip();
+
+
+});
