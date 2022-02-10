819982be54f0b7a9a7a94a9441b2635bcbdb8fc8
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-02-10 16:57:35 -0600

    puliendo detalles

--- src/index.js
+++ src/index.js
@@ -18,30 +18,3 @@ var completo = document.getElementById('completo');
 completo.addEventListener('click', () => {
     video.requestFullscreen();
 })
-
-function skip() {
-    $('.skip_button').on('click', function() {
-        var vid = $("#video1")[0];
-        var vidDuration = Math.floor(vid.duration);
-        skipTime(vidDuration);
-        console.log(vidDuration);
-
-    })
-
-}
-
-function skipTime(time) {
-    var vid = $("#video1")[0];
-    vid.play();
-    vid.pause();
-    vid.currentTime = time;
-    vid.play();
-    console.log(vid.duration);
-};
-
-$(function() {
-
-    skip();
-
-
-});
